7d4f6c6c46c220201ca63975dec845185eed519c
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-16 20:28:26 +0200

    use the new middleware

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 
 var register = require('./routes/register');
+var messages = require('./lib/messages');
 
 var app = express();
+// use middleware
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(messages);
 
 app.get('/', function(req, res) {
   res.send('Hello world!');
--- routes/register.js
+++ routes/register.js
@@ -1,3 +1,5 @@
+var User = require('../lib/user');
+
 exports.form = function(req, res) {
   res.render('register', { title: 'Register' });
-}
+};
