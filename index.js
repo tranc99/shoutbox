f5f03e6b613eff6cda9623533856474bdd291849
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-16 21:20:20 +0200

    authenticate the user

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 
-var register = require('./routes/register');
-var messages = require('./lib/messages');
 
 var app = express();
 // use middleware
-app.use(express.methodOverride());
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+
 app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+app.use(express.session({secret: "hello"}));
+
+var register = require('./routes/register');
+var messages = require('./lib/messages');
+
 app.use(messages);
+app.use(express.static(__dirname + '/views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.send('Hello world!');
@@ -19,3 +27,4 @@ app.get('/register', register.form);
 app.post('/register', register.submit);
 
 app.listen(3000);
+console.log('Express running on port 3000');
