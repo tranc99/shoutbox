561898fc382bb33ecf1718c51bd8f9739cf275dd
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-16 18:17:45 +0200

    add registration routes

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+
+var register = require('./routes/register');
+
 var app = express();
 
 app.get('/', function(req, res) {
   res.send('Hello world!');
 });
+
+// registration
+app.get('/register', register.form);
+app.post('/register', register.submit);
 
 app.listen(3000);
